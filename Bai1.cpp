ccff370a365d875fc5345ad185eb279ceb0c1575
NguyenThanhHoa2002 <85250249+NguyenThanhHoa2002@users.noreply.github.com>
2023-03-06 02:31:21 +0700

    Add files via upload

---
+++ Bai1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+const int ROWS = 3;
+const int COLS = 3;
+
+bool is_lo_shu_magic_square(int arr[][COLS])
+{
+    // Tính tổng các phần tử trên đường chéo chính
+    int main_diag_sum = arr[0][0] + arr[1][1] + arr[2][2];
+    // Tính tổng các phần tử trên đường chéo phụ
+    int other_diag_sum = arr[0][2] + arr[1][1] + arr[2][0];
+    // Nếu tổng các phần tử trên đường chéo chính và đường chéo phụ không bằng nhau.
+    if (main_diag_sum != other_diag_sum) {
+        return false;
+    }
+    // Tính tổng các phần tử trên mỗi hàng
+    int row_sum = 0;
+    for (int i = 0; i < ROWS; i++) {
+        int curr_row_sum = 0;
+        for (int j = 0; j < COLS; j++) {
+            curr_row_sum += arr[i][j];
+        }
+        // Nếu tổng các phần tử trên một hàng khác với tổng các phần tử trên đường chéo chính và đường chéo phụ, mảng không phải là ma trận vuông
+        if (curr_row_sum != main_diag_sum) {
+            return false;
+        }
+    }
+    // Tính tổng các phần tử trên mỗi cột
+    int col_sum = 0;
+    for (int j = 0; j < COLS; j++) {
+        int curr_col_sum = 0;
+        for (int i = 0; i < ROWS; i++) {
+            curr_col_sum += arr[i][j];
+        }
+        // Nếu tổng các phần tử trên một cột khác với tổng các phần tử trên đường chéo chính và đường chéo phụ, mảng không phải ma trậ vuông
+        if (curr_col_sum != main_diag_sum) {
+            return false;
+        }
+    }
+    // Nếu mảng thỏa mãn các điều kiện trên, mảng là ma trậ vuông
+    return true;
+}
+
+int main()
+{
+    // Khởi tạo mảng
+    int arr[ROWS][COLS] = { {4, 9, 2}, {3, 5, 7}, {8, 1, 6} };
+    // Kiểm tra xem mảng có phải là Mã Lục Thư ma trậ vuông
+    if (is_lo_shu_magic_square(arr)) {
+        cout << "Day la môt ma tran vuong\n";
+    } else {
+        cout << "Day khong phai la ma tran vuong\n";
+    }
+    return 0;
+}
